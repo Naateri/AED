4d39b4b9edd4f9389492e3d43d13ebb3321986d5
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-09-15 13:44:25 -0500

    Ordenamiento con puntero a funcion

--- pointerToFunction.cpp
+++ pointerToFunction.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 template <class T>
-T* generarArray(int size){
+T* generarArray(unsigned long long size){
 	srand(time(NULL));
 	T* ret = new T[size];
 	T* a = ret;
 	for(int i = 0; i<size; a++, i++){
-		*a = T(rand());
+		*a = T(rand()) % 1000000;
 	}
 	return ret;
 }
@@ -28,10 +28,10 @@ void imprimir(T *a, T *b){
 
 template <class T>
 void swap(T *p, T *q){
-	T* temp;
-	*temp = *p;
+	T temp;
+	temp = *p;
 	*p = *q;
-	*q = *temp;
+	*q = temp;
 }
 
 template <class T>
@@ -49,38 +49,83 @@ void cocktail(T *a, T *fin, bool (*p) (T, T)){
 	T *aux;
 	aux = a;
 	bool compro = 1;
+	T *auxF = fin;
 	while (compro){
-		cout << "bucleee\n";
 		compro = 0;
 		for(a; a < fin; a++){
-			cout << "fooooor1\n";
 			if(p(*a, *(a+1))){
-				cout << "swaaaaaaaaaap1\n";
 				swap<T>(*a, *(a+1));
 				compro = 1;
 			}
 		}
 		for(a; fin != aux; fin--){
-			cout << "foooor2\n";
 			if(!(p(*fin, *(fin-1)))){
 				swap<T>(*fin, *(fin-1));
 				compro = 1;
 			}
 		}
-		cout << "FUERA FORSSSS\n";
 		swap<T>(*a,*fin);
-		a++;
-		fin--;
+		aux++;
+		a = aux;
+		auxF--;
+		fin = auxF;
 	}
 }
 
-int main(int argc, char *argv[]) {
+template <class T>
+void a1000Elem(bool (*p) (T, T)){
+	double elapsedTime;
 	int* array1000 = generarArray<int>(1000);
-	bool (*p) (int, int) = &menor<int>;
-	//p = menor<int>;
-	imprimir<int>(array1000, array1000+999);
+	clock_t begin = clock();
 	cocktail<int>(array1000, array1000+999, p);
+	clock_t end = clock();
+	elapsedTime = double(end-begin) / CLOCKS_PER_SEC;
+	cout << "Array 1000 elementos: " << elapsedTime << endl;
 	delete array1000;
+}
+
+template <class T>
+void a100kElem(bool (*p) (T, T)){
+	double elapsedTime;
+	int* array100k = generarArray<int>(100000);
+	clock_t begin = clock();
+	cocktail<int>(array100k, array100k+99999, p);
+	clock_t end = clock();
+	elapsedTime = double(end-begin) / CLOCKS_PER_SEC;
+	cout << "Array 100 000 elementos: " << elapsedTime << endl;
+	delete array100k;
+}
+
+template <class T>
+void a1MElem(bool (*p) (T, T)){
+	double elapsedTime;
+	int* array1M = generarArray<int>(1000000);
+	clock_t begin = clock();
+	cocktail<int>(array1M, array1M+999999, p);
+	clock_t end = clock();
+	elapsedTime = double(end-begin) / CLOCKS_PER_SEC;
+	cout << "Array 1 000 000 elementos: " << elapsedTime << endl;
+	delete array1M;
+}
+
+template <class T>
+void a100MElem(bool (*p) (T, T)){
+	double elapsedTime;
+	int* array100M = generarArray<int>(100000000);
+	clock_t begin = clock();
+	cocktail<int>(array100M, array100M+99999999, p);
+	clock_t end = clock();
+	elapsedTime = double(end-begin) / CLOCKS_PER_SEC;
+	cout << "Array 100 000 000 elementos: " << elapsedTime << endl;
+	delete array100M;
+}
+
+int main(int argc, char *argv[]) {
+	bool (*p) (int, int) = &menor<int>;
+	a1000Elem<int>(p);
+	a100kElem<int>(p);
+	a1MElem<int>(p);
+	a100MElem<int>(p);
 	return 0;
 }
 
