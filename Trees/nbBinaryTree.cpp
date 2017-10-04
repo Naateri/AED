bd195bdc25702f442b85991e686b137d951f3ab8
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-10-03 23:59:25 -0500

    Tree, erase now works B|

--- Trees/nbBinaryTree.cpp
+++ Trees/nbBinaryTree.cpp
@@ -16,16 +16,16 @@ class BinTree{
 private:
 	void erase0(TNode<T>**& p);
 	void erase1(TNode<T>**& p);
-	void erase2();
+	void erase2(TNode<T>**& p);
 public:
 	TNode<T>* root;
 	BinTree();
 	bool find(T x, TNode<T>**& p);
 	bool insert(T x);
 	bool erase(T x);
-	void preorder();
+	void preorder(TNode<T>* cur);
 	void inorder(TNode<T>* cur);
-	void postorder();
+	void postorder(TNode<T>* cur);
 };
 
 template <class T>
@@ -70,17 +70,65 @@ void BinTree<T>::erase1(TNode<T>**& p){
 	}
 	delete *p;
 	(*p) = temp;
-	
+}
+
+template <class T>
+void BinTree<T>::erase2(TNode<T>**& p){
+	TNode<T>* temp, **tmp;
+	T tempVal;
+	temp = (*p)->left;
+	while ( temp->right ){
+		temp = temp->right;
+	}
+	tempVal = (*p)->val;
+	(*p)->val = temp->val;
+	temp->val = tempVal;
+	tmp = &temp;
+	if ( !(temp)->left ){
+		erase0(tmp);
+	} else {
+		erase1(tmp);
+	}
 }
 
 template <class T>
 bool BinTree<T>::erase(T x){
 	TNode<T> **p;
 	if (!find(x, p)) return 0;
-	if ( !(*p)->left && !(*p)->right){
-		erase0(p);
-	} else if ( (!(*p)->left && (*p)->right) || (!(*p)->right && (*p)->left)){
-		erase1(p);
+	if ((*p)->right){
+		if((*p)->left){ //tiene 2 hijos
+			TNode<T> **tmp;
+			T tempVal;
+			tmp = &(*p)->left;
+			while ( (*tmp)->right ){
+				tmp = &(*tmp)->right;
+			}
+			tempVal = (*p)->val;
+			(*p)->val = (*tmp)->val;
+			(*tmp)->val = tempVal;
+			if ((*tmp)->left){
+				delete *tmp;
+				*tmp = (*tmp)->left;
+			} else {
+				delete *tmp;
+				*tmp = 0;
+			}
+		} else { //solo tiene el hijo derecho
+			TNode<T>* temp;
+			temp = (*p)->right;
+			delete *p;
+			*p = temp;
+		}
+	} else {
+		if (!((*p)->right)){ //solo tiene el hijo izquierdo
+			TNode<T>* temp;
+			temp = (*p)->left;
+			delete *p;
+			*p = temp;
+		} else { //no tiene hijos
+			delete *p;
+			*p = 0;
+		}
 	}
 	return 1;
 }
@@ -90,6 +138,13 @@ void BinTree<T>::inorder(TNode<T>* cur){
 	cout << cur->val << " ";
 	if (cur->left) inorder(cur->left);
 	if (cur->right) inorder(cur->right);
+}
+
+template <class T>
+void BinTree<T>::postorder(TNode<T>* cur){
+	if (cur->left) postorder(cur->left);
+	if (cur->right) postorder(cur->right);
+	cout << cur->val << " ";
 }
 
 int main(int argc, char *argv[]) {
@@ -104,10 +159,10 @@ int main(int argc, char *argv[]) {
 	myTree.insert(95);
 	myTree.inorder(myTree.root);
 	cout << endl;
-	/*myTree.erase(77);
+	myTree.erase(77);
 	myTree.inorder(myTree.root);
-	cout << endl;*/
-	myTree.erase(94);
+	cout << endl;
+	myTree.erase(51);
 	myTree.inorder(myTree.root);
 	cout << endl;
 	return 0;
