e5dd519948b8aa84fda480b3f11e795c341d67de
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-11-04 15:58:21 -0500

    Aun falta Dijkstra

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -116,32 +116,47 @@ void CGraph<N, E>::draw(){
 template <class N, class E>
 uint CGraph<N, E>::Dijkstra(Node* start, Node* goal){
 	vector<Dist2Node<int> > distances; //Arreglo de distancias a que nodo
-	bool seen[m_nodes.size()];
-	distances[0].distance = 0;
-	distances[0].Node = 0;
-	seen[0] = 1;
-	int i;
-	for(i = 1; i < m_nodes.size()-1; i++){
-		distances[i].distance = -1; //-1 = INFINITE
-		distances[i].Node = 0;
+	bool seen[m_nodes.size()]; //Para saber si ya paso 
+	Dist2Node<int> s;
+	s.distance = -1; //-1 = infinite
+	s.Node = 0;
+	for (int i = 0; i < m_nodes.size(); i++){
+		distances.push_back(s);
 		seen[i] = 0;
 	}
-	i = 1;
-	for(typename list<Edge*>::iterator it = start->m_edges.begin(); it != start->m_edges.end(); it++){ //recorrer aristas del nodo
-		if ( ! (*it)->m_dir){
-			if (start == (*it)->m_nodes[0]){  //guardamos las distancias
-				distances[i].Node = (*it)->m_nodes[1];
-			} else {
-				distances[i].Node = (*it)->m_nodes[0];
+	distances[findInVector<Node*>(m_nodes, start)].distance = 0;
+	distances[findInVector<Node*>(m_nodes, start)].Node = start;
+	seen[findInVector<Node*>(m_nodes, start)] = 1;
+	//Todo lo de arriba: Llenando los valores iniciales
+	int mini = 0, index;
+	Node* cur, *minDist;
+	for(int i = 0; i < m_nodes.size() - 1; i++){
+		mini = -1; //inicializa un nuevo minimo
+		for(typename list<Edge*>::iterator it = cur->m_edges.begin(); it != cur->m_edges.end(); it++){ //recorrer aristas del nodo
+			//pregunta si ya vio ese nodo ///REVISAR ESE OR
+			if ( seen[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])] || seen[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])] ) continue;
+			if (cur == (*it)->m_nodes[0]){  //guardamos las distancias
+				if (distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])].distance == -1){ //si nadie ha pasado por ahi
+					distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])].Node = (*it)->m_nodes[0];
+					distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])].distance = (*it)->m_data;
+				} else {
+					//distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[0])].Node = (*it)->m_nodes[1];
+					distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[0])].distance 
+						= min( (*it)->m_data, distance[findInVector<Node*>(m_nodes, cur)] );
+				}
+			/*} else {
+				distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])].Node = (*it)->m_nodes[0];
+				distances[findInVector<Node*>(m_nodes, (*it)->m_nodes[1])].distance = (*it)->m_data;
+			}*/
+			if ( (*it)->m_data < mini || mini == -1) {
+				mini = (*it)->m_data;
+				minDist = *it;
 			}
-			distances[i].distance = (*it)->m_data;
+			}
 		}
-		seen[i++] = 1;
+		seen[findInVector<Node*>(m_nodes, minDist)] = 1;
 	}
-	i = 1;
-	
 }
-
 int main(int argc, char *argv[]) {
 	/*CGraph<char, int> g;
 	g.insertNode('A');
