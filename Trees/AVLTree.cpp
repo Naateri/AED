27622e02fa23094c0be309f85efef912f1b4c05e
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-11-19 20:48:37 -0500

    Yæsta el insertado, falta borrado

--- Trees/AVLTree.cpp
+++ Trees/AVLTree.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 typedef unsigned int uint;
-typedef unsigned short ushort;
-
 
 //1, 8, 3, 7, 2, 9, 4, 5, 6
 
@@ -39,17 +37,27 @@ public:
 	void inorder(CNode<T>* p);
 	//private:
 	CNode<T>** Rep(CNode<T>** p);
-	short getHeight(CNode<T>* p);
+	short getHeight(CNode<T>* p, T x);
 	void printTree(CNode<T>* p);
 private:
 	bool findHeight(CNode<T>**& p, short& h);
-	void updateHeights();
+	void updateHeights(T x);
 	list< CNode<T>* > path;
+	short posInList(T val);
 	void RR(CNode<T>** p);
 	void LL(CNode<T>** p);
 	void LR(CNode<T>** p);
 	void RL(CNode<T>** p);
 };
+
+template <class T, class C>
+short AVL<T,C>::posInList(T val){
+	short i = 1;
+	typename list<CNode<T>* >::iterator it;
+	for(it = path.begin() ; it != path.end() && (*it)->m_x != val; it++, i++);
+	if (it == path.end() ) return -1;
+	return i;
+}
 
 template <class T, class C>
 void AVL<T,C>::printTree(CNode<T>* p){
@@ -84,7 +92,7 @@ void AVL<T,C>::RR(CNode<T>** p){
 	typename list<CNode<T>*>::iterator it = path.end();
 	bool LR = 0, RL = 0;
 	if ( (*p)->m_nodes[1] && (*p)->m_nodes[1]->m_nodes[1] && (*p)->m_nodes[1]->m_nodes[1]->height == -1) LR = 1;
-	else if ( (*p)->height  == 1) RL = 1;
+	if ( (*p)->height  == 1) {RL = 1; LR = 0;}
 	//it++;
 	while ( (*it) != (*p) ){
 		it--;
@@ -95,7 +103,7 @@ void AVL<T,C>::RR(CNode<T>** p){
 		it++; //regresando a la posicion
 	}
 	CNode<T>* gfather = *(it++);
-	if (LR && !(*it)->m_nodes[1]) father = *(++it);
+	if (LR) father = *(++it);
 	else father = *(it);
 	//if (!father->m_nodes[0] && !father->m_nodes[1] ) RL = 1;
 	CNode<T> *bl = father->m_nodes[0];
@@ -105,7 +113,7 @@ void AVL<T,C>::RR(CNode<T>** p){
 	else {
 		if (RL){
 			temp->m_nodes[0] = father;
-		}
+		} else if (LR && father->height == 1) temp->m_nodes[0] = father;
 		else temp->m_nodes[1] = father;
 	}
 }
@@ -115,7 +123,7 @@ void AVL<T,C>::LL(CNode<T>** p){
 	bool LR = 0, RL = 0;
 	typename list<CNode<T>*>::iterator it = path.end();
 	if ((*p)->m_nodes[0] && (*p)->m_nodes[0]->m_nodes[0] && (*p)->m_nodes[0]->m_nodes[0]->height == 1 ) RL = 1;
-	else if ( (*p)->height == -1) LR = 1;
+	if ( (*p)->height == -1) {LR = 1; RL = 0;}
 	while ( (*it) != (*p)){
 		it--;
 	}
@@ -138,6 +146,7 @@ void AVL<T,C>::LL(CNode<T>** p){
 		if (LR) {
 			temp->m_nodes[1] = father;
 		}
+		else if (RL && father->height == -1) temp->m_nodes[1] = father;
 		else temp->m_nodes[0] = father;
 	}
 }
@@ -161,20 +170,26 @@ CNode<T>** AVL<T,C>::Rep(CNode<T>** p){
 }
 
 template <class T, class C>
-short AVL<T,C>::getHeight(CNode<T>* p){
+short AVL<T,C>::getHeight(CNode<T>* p, T x){
+	if (p->m_x == x) return 0;
 	short left, right;
 	CNode<T> *temp = p;
-	for(left = 0;  temp && (temp)->m_nodes[0]; temp = (temp)->m_nodes[0], left++);
-	if (temp->m_nodes[1] && temp != p) left++;
-	for(right = 0, temp = p; temp && (temp)->m_nodes[1]; temp = temp->m_nodes[1], right++);
-	if (temp->m_nodes[0] && temp != p) right++;
+	if (x > p->m_x){
+		right = path.size() - posInList(p->m_x);
+		for(left = 0;  temp && (temp)->m_nodes[0]; temp = (temp)->m_nodes[0], left++);
+		if (temp->m_nodes[1] && temp != p) left++;
+	} else {
+		left = path.size() - posInList(p->m_x);
+		for(right = 0, temp = p; temp && (temp)->m_nodes[1]; temp = temp->m_nodes[1], right++);
+		if (temp->m_nodes[0] && temp != p) right++;
+	}
 	return right - left;
 }
 
 template <class T, class C>
-void AVL<T,C>::updateHeights(){
+void AVL<T,C>::updateHeights(T x){
 	for(typename list<CNode<T>* >::iterator it = path.begin(); it != path.end(); it++){
-		(*it)->height = getHeight(*it);
+		(*it)->height = getHeight(*it, x);
 	}
 }
 
@@ -202,7 +217,7 @@ bool AVL<T,C>::insert(T x){
 	}
 	*p = new CNode<T>(x);
 	path.push_back(*p);
-	updateHeights();
+	updateHeights(x);
 	findHeight(p, h);
 	switch(h){
 	case 2:
@@ -221,7 +236,7 @@ bool AVL<T,C>::insert(T x){
 		Switch = 0;
 		break;
 	}
-	if (Switch) updateHeights();
+	if (Switch) updateHeights(x);
 	path.clear();
 	return 1;
 }
@@ -252,7 +267,7 @@ void AVL<T,C>::inorder(CNode<T>* p){
 int main(int argc, char *argv[]) {
 	AVL<int, Menor <int> > Tree;
 	int xd;
-	/*Tree.insert(5);
+	Tree.insert(5);
 	Tree.insert(7);
 	Tree.insert(9);
 	Tree.printTree(Tree.m_root);
@@ -287,14 +302,19 @@ int main(int argc, char *argv[]) {
 	Tree.printTree(Tree.m_root);
 	cin >> xd;
 	cout << endl;
-	Tree.insert(6);*/
+	Tree.insert(6);
+	Tree.insert(8);
+	Tree.insert(19);
+	Tree.insert(21);
+	Tree.insert(24);
+	Tree.insert(22);
 	/*Tree.insert(5); //LR Base Test
 	Tree.insert(9);
 	Tree.insert(7);*/
 	/*Tree.insert(6); //RL Base Test
 	Tree.insert(3);
 	Tree.insert(5);*/
-	Tree.insert(1);
+	/*Tree.insert(1); //ARBOL PRUEBA GUSTAVO
 	Tree.insert(8);
 	Tree.insert(3);
 	Tree.insert(7);
@@ -302,6 +322,7 @@ int main(int argc, char *argv[]) {
 	Tree.insert(9);
 	Tree.insert(4);
 	Tree.insert(5);
+	Tree.insert(6);*/
 	Tree.printTree(Tree.m_root);
 	return 0;
 }
