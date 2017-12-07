16f5461fdfaab1823e4ed9d870223f4e1d81cf1d
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-12-07 11:43:16 -0500

    Hash Table: Functor

--- HashTable/HashTable.cpp
+++ HashTable/HashTable.cpp
@@ -9,15 +9,14 @@ class HashTable{
 	E m_table[size];
 	F m_f;
 public:
-	T* find(T x);
+	bool find(T x, T*& t, int& p);
 	bool insert(T x);
 	bool remove(T x);
 };
 
 template <class T, class F, class E, int size>
-T* HashTable<T, F, E, size>::find(T x){
-	m_table[m_f(x) % size].print();
-	return m_table[m_f(x) % size].find(x);
+bool HashTable<T, F, E, size>::find(T x, T*& t, int& p){
+	return m_table[m_f(x) % size].find(x, t, p);
 }
 
 template <class T, class F, class E, int size>
@@ -34,66 +33,65 @@ template <class T>
 class CLA{
 	list<T> m_list;
 public:
-	T* find(T x);
+	bool find(T x, T*& t, int& p);
 	bool insert(T x);
 	bool remove(T x);
 	void print(){
 		for(typename list<T>::iterator it = m_list.begin(); it != m_list.end(); it++){
 			cout << (*it) << " ";
 		}
+		cout << endl;
 	}
 };
 
 template <class T>
-T* CLA<T>::find(T x){
+bool CLA<T>::find(T x, T*& t, int& p){
+	if (m_list.empty() ) return 0;
 	typename list<T>::iterator it = m_list.begin();
-	T x1;
-	cout << "m_list.begin(): " << *(m_list.begin()) << endl;
-	for( ; it != m_list.end() && (*it) != x; it++){
-		cout << "*it: " << *it << endl;
+	p = 0;
+	for( ; it != m_list.end() && (*it) != x; it++, p++){
+		;
 	}
-	x = *it;
-	if (x) return &x;
-	return 0;
+	t = &(*it);
+	if (*t != x) *t = 0;
+	return (x == *it);
 }
 
 template <class T>
 bool CLA<T>::insert(T x){
-	if (find (x) ) return 0;
+	int p;
+	T* temp;
+	if (find (x, temp, p) ) return 0;
 	m_list.push_back(x);
+	return 1;
 }
 
+template <class T>
+bool CLA<T>::remove(T x){
+	int p;
+	T* temp;
+	if (!find(x, temp, p) ) return 0;
+	m_list.remove(m_list.begin() + p);
+	return 1;
+}
 
-struct Person{
-	string first_name;
-	string last_name;
-	int age;
-	ostream& operator<< (ostream& out){
-		out << "Name: " << first_name << " " << last_name << endl;
-		out << "Age: " << age << endl;
-		return out;
-	}
-	Person(string fn, string ln, int ag){
-		this->first_name = fn;
-		this->last_name = ln;
-		this->age = ag;
-	}
-};
-
-template <int size>
+template <class T, int size>
 struct HashFunction{
-	int operator()(int x){
+	T operator()(T x){
 		return (x+50) % size;
 	}
 };
 
 int main(int argc, char *argv[]) {
-	int *x;
-	HashTable<int, HashFunction<13>, CLA<int>, 13> Hash;
+	int* x, y;
+	HashTable<int, HashFunction<int, 5>, CLA<int>, 5> Hash;
 	Hash.insert(5);
 	Hash.insert(8);
-	cout << "Ma find\n";
-	cout << Hash.find(5);
+	Hash.insert(9);
+	Hash.insert(-3);
+	Hash.insert(3);
+	Hash.find(15, x, y);
+	cout << *x << endl;
 	return 0;
 }
 
